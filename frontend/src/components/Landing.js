9d9d5f9c9a06238bfc698c03d69d0f7543567680
ahmedfahadpk <57640040+ahmedfahadpk@users.noreply.github.com>
2020-05-07 11:03:36 +0300

    resolving conflicts

---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ContactForm from "./ContactForm";
+import Login from "./Login";
+import Signup from "./Signup";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="#home">Meal Plan</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/login">Login</Link>
+            <Nav.Link>
+            <Nav.Link>
+              <Link to="/signup">Signup</Link>
+            <Nav.Link>
+            <Nav.Link>
+              <Link to="/about">About</Link>
+            <Nav.Link>
+            </Nav.Link>
+              <Link to="/contact">Contact us</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/contact">
+            <ContactForm />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <ContactForm />;
+}

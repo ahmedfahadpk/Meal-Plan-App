9eca2b46380bb2268a66edf0a0017885c725d8a5
oona-k <59806531+oona-k@users.noreply.github.com>
2020-05-12 15:27:01 +0300

    Added Font Awesome icons

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,8 +1,17 @@
 import React from "react";
+
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 import Button from "react-bootstrap/Button";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUtensils,
+  faSearch,
+  faBookOpen,
+  faCarrot,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "./About.css";
 
@@ -14,6 +23,10 @@ const About = () => {
         <CardGroup>
           <Card className="aboutCard">
             <Card.Body>
+              <FontAwesomeIcon
+                icon={faSearch}
+                style={{ fontSize: "30px", marginBottom: "15px" }}
+              />
               <Card.Title>Choose Filters</Card.Title>
               <Card.Text>
                 Search recipes by selecting ingredient, calories, diet and
@@ -23,12 +36,20 @@ const About = () => {
           </Card>
           <Card className="aboutCard">
             <Card.Body>
+              <FontAwesomeIcon
+                icon={faBookOpen}
+                style={{ fontSize: "30px", marginBottom: "15px" }}
+              />
               <Card.Title>Pick Recipe</Card.Title>
               <Card.Text> Choose one recipe that you like.</Card.Text>
             </Card.Body>
           </Card>
           <Card className="aboutCard">
             <Card.Body>
+              <FontAwesomeIcon
+                icon={faCarrot}
+                style={{ fontSize: "30px", marginBottom: "15px" }}
+              />
               <Card.Title>Cook</Card.Title>
               <Card.Text>Cook your meal following the recipe.</Card.Text>
             </Card.Body>
@@ -36,6 +57,10 @@ const About = () => {
 
           <Card className="aboutCard">
             <Card.Body>
+              <FontAwesomeIcon
+                icon={faUtensils}
+                style={{ fontSize: "30px", marginBottom: "15px" }}
+              />
               <Card.Title>Eat and Repeat</Card.Title>
               <Card.Text> Enjoy your meal and welcome back!</Card.Text>
             </Card.Body>
